6e9d1a97dabcd73c1952c38b6b2a3d7c6abcfe22
BrunoPlasser <148269476+BrunoPlasser@users.noreply.github.com>
2024-04-29 23:24:16 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+
+document.getElementById("playButton").addEventListener("click", bt);
+
+function bt() {
+    window.location.href = "https://discord.gg/fCWK2mrNS";
+}
+
+document.getElementById("playButton").addEventListener("click", Qb);
+
+function Qb() {
+    window.location.href = "https://brunoplasser.github.io/quiz-the-road/";
+}
+
+document.getElementById("playButton").addEventListener("click", Qb);
+
+function wb() {
+    window.location.href = "https://brunoplasser.github.io/wiki_TR/";
+}
+
+document.getElementById("WikiButton").addEventListener("click", wb);
+
+
